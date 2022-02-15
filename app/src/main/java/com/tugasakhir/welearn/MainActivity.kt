c92a9403b8d738e423ccd8c8266c76a4dec928ff
Haris Wicaksono <hariswicaksono7@gmail.com>
2022-02-15 22:28:32 +0700

    error

--- app/src/main/java/com/tugasakhir/welearn/MainActivity.kt
+++ app/src/main/java/com/tugasakhir/welearn/MainActivity.kt
@@ -2,15 +2,35 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.tugasakhir.welearn.databinding.ActivityMainBinding
 
  class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+
+        ), binding.drawerLayout)
+
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment), appBarConfiguration)
+        binding.navView.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
+
+     override fun onSupportNavigateUp(): Boolean {
+         return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) ||
+                 super.onSupportNavigateUp()
+     }
 }
